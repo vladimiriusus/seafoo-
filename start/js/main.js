daba5446c7c6ad37f70b9e05526a25a33bce4858
vladimiriusus <tatar-vova@ukr.net>
2021-12-03 15:35:01 +0200

    first

---
+++ start/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+	$('.header-slider').slick({
+		dots: true,
+		prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.svg" alt=""></button>',
+		nextArrow:'<button type="button" class="slick-next"><img src="img/next.svg" alt=""></button>',
+		vertical:true
+	})
+
+});
