6cdcc61e45bf2b7671b28e14fe1b360ad2ee964e
vladimiriusus <tatar-vova@ukr.net>
2021-12-15 19:20:10 +0200

    15.02

--- start/js/main.js
+++ start/js/main.js
@@ -5,21 +5,26 @@ $(function(){
 		prevArrow: '<button type="button" class="slick-prev"><img src="img/prev.svg" alt=""></button>',
 		nextArrow:'<button type="button" class="slick-next"><img src="img/next.svg" alt=""></button>',
 		vertical:true
-	})
+	});
 
 
 
 $('.product__name').slick({
-	slidesToShow: 1,
+	slidesToShow: 6,
 	focusOnSelect: true,
 	slidesToScroll: 1,
-	asNavFor: '.product__content'
+	asNavFor: '.product__content',
+	vertical: true,
+	prevArrow: '<button type="button" class="product-prev"><img src="img/product-prev.svg" alt=""></button>',
+	nextArrow:'<button type="button" class="product-next"><img src="img/product-next.svg" alt=""></button>',
  });
+
  $('.product__content').slick({
 	slidesToShow: 1,
 	slidesToScroll: 1,
 	asNavFor: '.product__name',
-fade: true
+	fade: true,
+	arrows: false
  });
  
 
